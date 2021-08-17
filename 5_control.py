2402362b4b5186cb61b0a81038115369f2bae9e5
hrant10 <hrantb410@hotmail.com>
2021-08-17 13:35:57 +1000

    commit

---
+++ 5_control.py
@@ -0,0 +1,11 @@
+#if statements
+
+print("type a number:")
+num1 = int(input())
+
+if num1 == 5:
+    print("you typed in 5.")
+elif num1 ==6:
+    print("You typed in 6.")
+else:
+    print("You didn't type 5!")
